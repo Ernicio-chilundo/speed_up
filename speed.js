1ba51cfaf2b34bfdb10423f07185beef158ae62f
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-07-08 08:51:11 +0200

    aprendendo a criar jogos

--- speed.js
+++ speed.js
@@ -2,8 +2,15 @@ var canvas = document.getElementById("my_canvas")
 
 var context = canvas.getContext("2d")
 
-context.lineWidth = 10
+context.fillStyle = "#000"
 context.fillRect(50 , 50 , 100 ,100)
 
-context.strokeStyle = "#aaa"
-context.strokeRect(50, 50 , 100 , 100)
+context.save()
+
+context.fillStyle ="#fff"
+context.fillRect(200 , 50 , 100 ,100)
+
+context.restore()
+
+context.fillRect(350,50,100,100)
+
